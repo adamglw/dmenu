823af074c2604ca406c32cc6d517570daecd9e48
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-03 21:39:53 +0100

    Minor font edits

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Ubuntu Nerd Font:weight=regular:pixelsize=17:antialias=true:hinting=true"
+	"Hack Nerd Font:weight=regular:pixelsize=15:antialias=true:hinting=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,7 +14,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000010", "#b39ddb" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 5;
+static unsigned int lines      = 9;
 
 /*
  * Characters not considered part of a word while deleting words
