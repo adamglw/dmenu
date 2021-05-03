750c730352d293bf5add933e68de938a3c5d0cc5
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-04 00:35:53 +0100

    Add min_width option for centered dmenu

--- config.h
+++ config.h
@@ -1,8 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1;             /* -b  option; if 0, dmenu appears at bottom          */
+static int fuzzy = 1;              /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int min_width = 500;        /* minimum width of centered dmenu                    */ 
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack Nerd Font:weight=regular:pixelsize=15:antialias=true:hinting=true"
