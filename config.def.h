785c80c4f9f6c500ea7a596a233ec7683769d7ae
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-08 18:02:58 +0100

    Edit colors

--- config.def.h
+++ config.def.h
@@ -3,25 +3,25 @@
 
 static int topbar = 1;             /* -b  option; if 0, dmenu appears at bottom          */
 static int fuzzy = 1;              /* -F  option; if 0, dmenu doesn't use fuzzy matching */
-static int min_width = 500;        /* minimum width of centered dmenu                    */ 
+static int min_width = 450;        /* minimum width of centered dmenu                    */ 
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack Nerd Font:weight=bold:size=11:antialias=true:hinting=true"
+	"Hack Nerd Font:weight=bold:pixelsize=15:antialias=true:hinting=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
                           /*     fg         bg       */
-	[SchemeNorm]          = { "#a39ec4", "#262626" },
-	[SchemeSel]           = { "#191919", "#615d7d" }, /* #655d92 = Ume dark */
-    [SchemeSelHighlight]  = { "#191919", "#ac8a8c" },
-    [SchemeNormHighlight] = { "#c49ea0", "#262626" },
-	[SchemeOut]           = { "#191919", "#747093" }, /* #747093 = Ajisai dark */
+	[SchemeNorm]          = { "#767676", "#262626" }, /* Gin    */
+	[SchemeSel]           = { "#8f8aac", "#262626" }, /* Ume    */
+    [SchemeSelHighlight]  = { "#a39ec4", "#262626" }, /* Ajisai */
+    [SchemeNormHighlight] = { "#c49ea0", "#262626" }, /* Ichigo */
+	[SchemeOut]           = { "#a39ec4", "#262626" }, /* Ajisai */
 };
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 10;
-static unsigned int columns    = 3;
+static unsigned int lines      = 9;
+static unsigned int columns    = 2;
 
 /* -h option: minimum height of a menu line */
 static unsigned int lineheight = 28;
@@ -34,4 +34,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 0;
