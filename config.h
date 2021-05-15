351c43985d9c961b4fb05243935648d1147f52ce
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-15 20:09:29 +0100

    Update colors and number of rows & columns

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;             /* -b  option; if 0, dmenu appears at bottom          */
 static int fuzzy = 1;              /* -F  option; if 0, dmenu doesn't use fuzzy matching */
-static int min_width = 450;        /* minimum width of centered dmenu                    */ 
+static int min_width = 600;        /* minimum width of centered dmenu                    */ 
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -12,16 +12,16 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
                           /*     fg         bg       */
-	[SchemeNorm]          = { "#767676", "#262626" }, /* Gin    */
-	[SchemeSel]           = { "#8f8aac", "#262626" }, /* Ume    */
-    [SchemeSelHighlight]  = { "#a39ec4", "#262626" }, /* Ajisai */
-    [SchemeNormHighlight] = { "#c49ea0", "#262626" }, /* Ichigo */
-	[SchemeOut]           = { "#a39ec4", "#262626" }, /* Ajisai */
+	[SchemeNorm]          = { "#9e9e9e", "#262626" }, 
+	[SchemeSel]           = { "#b9b9b9", "#303030" }, 
+    [SchemeSelHighlight]  = { "#85b785", "#303030" }, 
+    [SchemeNormHighlight] = { "#b78380", "#262626" }, 
+	[SchemeOut]           = { "#ce8c84", "#303030" },
 };
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 9;
-static unsigned int columns    = 2;
+static unsigned int lines      = 13;
+static unsigned int columns    = 3;
 
 /* -h option: minimum height of a menu line */
 static unsigned int lineheight = 28;
@@ -34,4 +34,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
